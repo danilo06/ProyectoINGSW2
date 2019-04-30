37a32cbfaf301facb18761888ee0264c3a387fab
elneg <elneg@LAPTOP-IQ504C3A>
2019-04-30 00:23:28 -0500

    Cambios Vista

--- src/main/java/ingsw/proyecto/SAGI/controllerView/ControllerVisitante.java
+++ src/main/java/ingsw/proyecto/SAGI/controllerView/ControllerVisitante.java
@@ -3,6 +3,9 @@ package ingsw.proyecto.SAGI.controllerView;
 import java.net.URL;
 import java.time.LocalTime;
 import java.util.ResourceBundle;
+
+import org.controlsfx.control.Notifications;
+
 import animatefx.animation.BounceIn;
 import animatefx.animation.FadeIn;
 import animatefx.animation.Flash;
@@ -12,9 +15,11 @@ import animatefx.animation.Swing;
 import animatefx.animation.Tada;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
@@ -25,6 +30,8 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTimePicker;
 
@@ -74,6 +81,10 @@ public class ControllerVisitante implements Initializable {
 
 	@FXML
 	private JFXDatePicker SeleccionarFecha;
+	
+	 @FXML
+	private Button btnPanel1RegistrarVisitante;
+	 
 
 	private Boolean fullScreen = false;
 
@@ -121,6 +132,25 @@ public class ControllerVisitante implements Initializable {
 	private void animated() {
 		new Jello(pnlStatus).play();
 	}
+	
+	@FXML
+	 void btnPanel1RegistrarVisitanteAction(ActionEvent event) {
+		Notifications notificationBuilder = Notifications.create()
+				.title("AVISO")
+				.text("Visitante Registrado")
+				.graphic(null)
+				.hideAfter(Duration.seconds(5))
+				.position(Pos.CENTER)
+				.onAction(new EventHandler<ActionEvent>() {
+					
+			public void handle(ActionEvent event) {
+				System.out.println("Registrado");
+			}
+		});
+		notificationBuilder.darkStyle();
+		notificationBuilder.showInformation();
+
+	 }
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
