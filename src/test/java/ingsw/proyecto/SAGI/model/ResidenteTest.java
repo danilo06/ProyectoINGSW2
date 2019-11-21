962aad8ec93b14b18b199c9c95c301b9d51775a4
Andres Felipe Barragan <abarragan02@uan.edu.co>
2019-11-21 18:37:43 -0500

    Listo

--- src/test/java/ingsw/proyecto/SAGI/model/ResidenteTest.java
+++ src/test/java/ingsw/proyecto/SAGI/model/ResidenteTest.java
@@ -13,13 +13,44 @@ class ResidenteTest extends TestCase{
 	@Test
 	public void testResidenteNumeroDocumento() {
 		
-		residente = new Residente.ResidenteBuilder().setApartamento("402").setCelular("3112541112").setEdad(0)
+		Residente residente = new Residente.ResidenteBuilder().setApartamento("402").setCelular("3112541112").setEdad(0)
 				.setGenero("Hombre").setNombre("Danilo Beleño").setNumDocumento("1030123123").setTipoDocumento("CC")
 				.setTipoResidente("Propietario").setTorre("TO1").build();
 		
-		assertTrue(residente.getNumDocumento().endsWith("1030123123"));
+		assertTrue(residente.getNumDocumento().equals(("1030123123")));
 		
 	}
+	
+	@Test
+	public void testResidenteApartamento() {
+		
+		Residente residente = new Residente.ResidenteBuilder().setApartamento("402").setCelular("3112541112").setEdad(0)
+				.setGenero("Hombre").setNombre("Danilo Beleño").setNumDocumento("1030123123").setTipoDocumento("CC")
+				.setTipoResidente("Propietario").setTorre("TO1").build();
+		
+		assertTrue(residente.getApartamento().equals(("402")));
+	}	
+		
+	@Test
+	public void testResidenteTipoResidente() {
+		
+		Residente residente = new Residente.ResidenteBuilder().setApartamento("402").setCelular("3112541112").setEdad(0)
+				.setGenero("Hombre").setNombre("Danilo Beleño").setNumDocumento("1030123123").setTipoDocumento("CC")
+				.setTipoResidente("Propietario").setTorre("TO1").build();
+	
+		assertFalse(residente.getTipoResidente().equals("Camionero"));
+	}
+	
+	@Test
+	public void testResidenteGenero() {
+		
+		Residente residente = new Residente.ResidenteBuilder().setApartamento("402").setCelular("3112541112").setEdad(0)
+				.setGenero("Hombre").setNombre("Danilo Beleño").setNumDocumento("1030123123").setTipoDocumento("CC")
+				.setTipoResidente("Propietario").setTorre("TO1").build();
+		
+		assertFalse(residente.getGenero().equals("Mascota"));
+	}
+	
 	
 
 }
