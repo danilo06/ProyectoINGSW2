80a111924cb691b971c1aa4783088bc27ceb467e
Danilo Beleño <dbeleno86@uan.edu.co>
2019-11-21 17:02:33 -0500

    Todo listo

---
+++ src/test/java/ingsw/proyecto/SAGI/model/ResidenteTest.java
@@ -0,0 +1,79 @@
+package ingsw.proyecto.SAGI.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ResidenteTest {
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testResidente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTipoDocumento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTipoDocumento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetEdad() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetEdad() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTipoResidente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTipoResidente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTorre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTorre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetApartamento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetApartamento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetGenero() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetGenero() {
+		fail("Not yet implemented");
+	}
+
+}
